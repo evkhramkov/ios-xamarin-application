2b635c1af1f3a07a04a5425c8484d2857f6da1ae
Evgeniy Khramkov <v-evkhra@microsoft.com>
2017-10-26 23:13:21 +0300

    init

---
+++ ios-xamarin-application/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ios-xamarin-application
+{
+	public class Item
+{
+    public string Id { get; set; }
+    public string Text { get; set; }
+    public string Description { get; set; }
+}
+}
---
+++ ios-xamarin-application/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ios-xamarin-application
+{
+	public class ItemDetailViewModel : BaseViewModel
+{
+    public Item Item { get; set; }
+    public ItemDetailViewModel(Item item = null)
+    {
+        Title = item?.Text;
+        Item = item;
+    }
+}
+}
