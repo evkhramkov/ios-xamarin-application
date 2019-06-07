35c4aa389f5a9a7b9eba261f7a7063efba12ec03
Evgenii Khramkov <evkhramk@microsoft.com>
2019-06-07 16:59:48 -0700

    add pre-build

---
+++ iOS/appcenter-pre-build.sh
@@ -0,0 +1,5 @@
+ls -la packages
+
+cat packages/Xamarin.Firebase.iOS.Crashlytics.3.10.9/build/Xamarin.Firebase.iOS.Crashlytics.targets
+
+sed -i '' '50,55d' packages/Xamarin.Firebase.iOS.Crashlytics.3.10.9/build/Xamarin.Firebase.iOS.Crashlytics.targets
