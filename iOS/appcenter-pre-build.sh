438c2d2df3f1d98241d5a8a444ff083f4e230206
Evgenii Khramkov <evkhramk@microsoft.com>
2019-06-07 17:09:14 -0700

    fix paths

--- iOS/appcenter-pre-build.sh
+++ iOS/appcenter-pre-build.sh
@@ -1,5 +1,5 @@
-ls -la ./
+ls -la /Users/vsts/.nuget/packages
 
-cat ./packages/Xamarin.Firebase.iOS.Crashlytics.3.10.9/build/Xamarin.Firebase.iOS.Crashlytics.targets
+cat /Users/vsts/.nuget/packages/Xamarin.Firebase.iOS.Crashlytics.3.10.9/build/Xamarin.Firebase.iOS.Crashlytics.targets
 
-sed -i '' '50,55d' ./packages/Xamarin.Firebase.iOS.Crashlytics.3.10.9/build/Xamarin.Firebase.iOS.Crashlytics.targets
+sed -i '' '50,55d' /Users/vsts/.nuget/packages/Xamarin.Firebase.iOS.Crashlytics.3.10.9/build/Xamarin.Firebase.iOS.Crashlytics.targets
