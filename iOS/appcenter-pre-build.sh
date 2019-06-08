918b67c5eadc1c57b2eaed7ea2e29375dafe94ae
Evgenii Khramkov <evkhramk@microsoft.com>
2019-06-07 17:02:55 -0700

    fix script

--- iOS/appcenter-pre-build.sh
+++ iOS/appcenter-pre-build.sh
@@ -1,5 +1,5 @@
-ls -la packages
+ls -la ./
 
-cat packages/Xamarin.Firebase.iOS.Crashlytics.3.10.9/build/Xamarin.Firebase.iOS.Crashlytics.targets
+cat ./packages/Xamarin.Firebase.iOS.Crashlytics.3.10.9/build/Xamarin.Firebase.iOS.Crashlytics.targets
 
-sed -i '' '50,55d' packages/Xamarin.Firebase.iOS.Crashlytics.3.10.9/build/Xamarin.Firebase.iOS.Crashlytics.targets
+sed -i '' '50,55d' ./packages/Xamarin.Firebase.iOS.Crashlytics.3.10.9/build/Xamarin.Firebase.iOS.Crashlytics.targets
