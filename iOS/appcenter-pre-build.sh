56357eb351ecb5c0c26d63f70766b005cf154e5c
Evgenii Khramkov <evkhramk@microsoft.com>
2019-06-07 23:04:59 -0700

    remove scripts

--- iOS/appcenter-pre-build.sh
+++
@@ -1,5 +0,0 @@
-ls -la ./
-
-ls -la /Users/vsts/.nuget/packages/xamarin.firebase.ios.crashlytics
-
-# sed -i '' '50,55d' /Users/vsts/.nuget/packages/xamarin.firebase.ios.crashlytics/build/Xamarin.Firebase.iOS.Crashlytics.targets
